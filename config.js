69d4bd2520893935cdaf1cc86b62319d355e950a
Jatinder Singh <jatindersingh33@gmail.com>
2017-03-12 15:17:55 +1100

    Added functionality of importing csv file
    Added config file
    
    Added dependency - babel for ES6

---
+++ config.js
@@ -0,0 +1,5 @@
+
+export default {
+  mongodbUri: 'mongodb://localhost:27017/test',
+  filePath: 'data/Australian_Post_Codes_Lat_Lon.csv'
+};
---
+++ server.js
@@ -0,0 +1,66 @@
+import express from 'express';
+import config from './config';
+import fs from 'fs';
+import {MongoClient} from 'mongodb';
+import assert from 'assert';
+import mongoose from 'mongoose';
+
+const server = express();
+
+// connect to database
+mongoose.connect(config.mongodbUri);
+
+var lineList = fs.readFileSync(config.filePath).toString().split('\n');
+lineList.shift();
+
+//creating schema for database
+var schemaKeyList = ['PostCode', 'Suburb', 'State', 'dc', 'type', 'lat', 'lon'];
+
+var PostCodeSchema = new mongoose.Schema({
+    PostCode: String,
+    Suburb: String,
+    State: String,
+    dc: String,
+    type: String,
+    lat: String,
+    lon: String
+});
+
+//create model for schema
+var PostDoc = mongoose.model('PostCodes', PostCodeSchema);
+
+function queryAllEntries () {
+    PostDoc.aggregate(
+        {$group: {_id: '$PostCode', oppArray: {$push: {
+            Suburb: '$Suburb',
+            State: '$State',
+            dc: '$dc',
+            type: '$type',
+            lat: '$lat',
+            lon: '$lon'
+            }}
+        }}, function(err, qDocList) {
+        //console.log(util.inspect(qDocList, false, 10));
+        process.exit(0);
+    });
+}
+
+
+function createDocRecurse (err) {
+    if (err) {
+        console.log(err);
+        process.exit(1);
+    }
+    if (lineList.length) {
+        var line = lineList.shift();
+        var doc = new PostDoc();
+        line.split(',').forEach(function (entry, i) {
+            doc[schemaKeyList[i]] = entry;
+        });
+        doc.save(createDocRecurse);
+    } else {
+        queryAllEntries();
+    }
+}
+
+createDocRecurse(null);
